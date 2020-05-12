2dec06a00e65cbce7d24675d7bda9010e87144be
Mohamed <m.boussakouk@student.bit-academy.nl>
2020-05-12 13:50:20 +0200

    Clean up

--- openssl-encrypt.php
+++ openssl-encrypt.php
@@ -15,21 +15,22 @@ $input = readline("Input: ");
 $output = encrypt_decrypt($option, $input);
 echo $output . PHP_EOL;
 
-function encrypt_decrypt($action, $string) 
-    {
-        $output = false;
-        $encrypt_method = "AES-256-CBC";
-        $secret_key = 'hVmYq3t6w9z$C&F)';
-        $secret_iv = 'gUkXp2s5';
-        // hash
-        $key = hash('sha256', $secret_key);    
-        // iv - encrypt method AES-256-CBC expects 16 bytes 
-        $iv = substr(hash('sha256', $secret_iv), 0, 16);
-        if ( $action == 'encrypt' ) {
-            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-            $output = base64_encode($output);
-        } else if( $action == 'decrypt' ) {
-            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
-        }
-        return $output;
+function encrypt_decrypt($action, $string)
+{
+    $output = false;
+    $encrypt_method = "AES-256-CBC";
+    $secret_key = 'hVmYq3t6w9z$C&F)';
+    $secret_iv = 'gUkXp2s5';
+
+    $key = hash('sha256', $secret_key);    
+    // iv - encrypt method AES-256-CBC expects 16 bytes 
+    $iv = substr(hash('sha256', $secret_iv), 0, 16);
+    if ( $action == 'encrypt' ) {
+        $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+        $output = base64_encode($output);
+    } else if ( $action == 'decrypt' ) {
+        $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
     }
+    
+    return $output;
+}
