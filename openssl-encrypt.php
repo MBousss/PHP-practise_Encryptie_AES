1dd10e0dfe71e7684b5f5cec101dad04f58516a5
Mohamed <m.boussakouk@student.bit-academy.nl>
2020-05-12 13:58:04 +0200

    Clean up

--- openssl-encrypt.php
+++ openssl-encrypt.php
@@ -25,10 +25,10 @@ function encrypt_decrypt($action, $string)
     $key = hash('sha256', $secret_key);    
     // iv - encrypt method AES-256-CBC expects 16 bytes 
     $iv = substr(hash('sha256', $secret_iv), 0, 16);
-    if ( $action == 'encrypt' ) {
+    if ($action == 'encrypt') {
         $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
         $output = base64_encode($output);
-    } else if ( $action == 'decrypt' ) {
+    } else if ($action == 'decrypt') {
         $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
     }
     
